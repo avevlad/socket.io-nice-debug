2d8ef41a8ddb378a387cb86d1480436b1ff68640
AveVlad <dev@vld.me>
2015-02-03 12:25:03 +0300

    add reboot btn

--- public/main.js
+++ public/main.js
@@ -14,5 +14,10 @@ $(function () {
     });
     $('[name="optionsUpdate"]').change(function () {
         updateLog = parseInt($(this).val());
-    })
+    });
+    $('.reboot').click(function () {
+        $.post('http://128.199.37.142:3000/api/v1/quest/reboot', function () {
+            console.log('reboot');
+        });
+    });
 });
